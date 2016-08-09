7cc40f9aefa406650c74de6933a1cf2139a32c93
dan523 <danwright523@gmail.com>
2016-08-09 14:49:20 -0400

    added basic routing

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/voting';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 
 const pair = ['Trainspotting', '28 Days Later'];
+
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
+
 ReactDOM.render(
-    <Voting pair={pair}/>,
-    document.getElementById('app')
+  <Router history={hashHistory}>{routes}</Router>,
+  document.getElementById('app')
 );
