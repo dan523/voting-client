f2e8a05a2ebb0402c07ae91232cbd3d0d46434ca
dan523 <danwright523@gmail.com>
2016-08-09 13:44:02 -0400

    Adding voting component

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,11 @@
 /// <reference path="../typings/index.d.ts" />
 
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+ReactDOM.render(
+    <Voting pair={pair}/>,
+    document.getElementById('app')
+);
