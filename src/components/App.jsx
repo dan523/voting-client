bca04155cddc5adcd194d8d28214d21428707848
dan523 <danwright523@gmail.com>
2016-08-09 14:58:01 -0400

    Adding results page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 /// <reference path="../../typings/index.d.ts" />
 
 import React from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
+
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 const pair = List.of('Trainspotting', '28 Days Later');
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {pair: pair});
+    return React.cloneElement(this.props.children, {
+        pair: pair,
+        tally: tally
+    });
   }
 });
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Voting from './components/voting';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
+import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
   <Route path="/" component={Voting} />
+  <Route path="/results" component={Results}/>
 </Route>;
 
 ReactDOM.render(
