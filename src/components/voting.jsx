5a93208b9ec80f64c32a07c09d27ea309f08f0b6
dan523 <danwright523@gmail.com>
2016-08-09 14:39:32 -0400

    Adding tests for Vote and Winner

--- src/components/voting.jsx
+++ src/components/voting.jsx
@@ -3,8 +3,10 @@
 import React from 'react';
 import Vote from './Vote';
 import Winner from './Winner';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
+    mixins: [PureRenderMixin],
     getPair: function () {
         return this.props.pair || [];
     },
