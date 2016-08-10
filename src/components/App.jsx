df0798339b081988a4418db1779bf8dd11c11cf1
dan523 <danwright523@gmail.com>
2016-08-10 16:31:49 -0400

    Removing defaults from App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {List, Map} from 'immutable';
-
-const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
-const pair = List.of('Trainspotting', '28 Days Later');
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {
-        pair: pair,
-        tally: tally
-    });
+    return this.props.children;
   }
 });
